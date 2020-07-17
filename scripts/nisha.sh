ac4b3e4441111b06c3efec8722b4e2e151abee34
Aniket Bhattacharyea <aniketmail669@gmail.com>
2020-07-17 17:49:15 +0530

    Add more options

--- scripts/nisha.sh
+++ scripts/nisha.sh
@@ -51,6 +51,12 @@ main() {
     tmux set-window-option -g window-status-current-format "#[fg=${background},bg=${orange},nobold,noitalics,nounderscore]${left_sep} #[bg=${orange},fg=${background}] #I ${left_sep2} #W #[fg=${orange},bg=${background},nobold,noitalics,nounderscore]${left_sep}"
     tmux set-window-option -g window-status-format "#[fg=${background},bg=${selection},nobold,noitalics,nounderscore]${left_sep} #[bg=${selection},fg=${foreground}] #I ${left_sep2} #W #[fg=${selection},bg=${background},nobold,noitalics,nounderscore]${left_sep}"
     tmux set-window-option -g window-status-separator ""
+
+    tmux set-window-option -g window-status-bell-style "#[fg=${background},bg=${selection},nobold,noitalics,nounderscore]${left_sep} #[bg=${selection},fg=${yellow}] #I ${left_sep2} #W #[fg=${yellow},bg=${background},nobold,noitalics,nounderscore]${left_sep}"
+
+    tmux set-option -g window-status-activity-style "#[fg=${background},bg=${selection},nobold,noitalics,nounderscore]${left_sep} #[bg=${selection},fg=${foreground}, blink] #I ${left_sep2} #W #[fg=${selection},bg=${background},nobold,noitalics,nounderscore]${left_sep}"
+
+    tmux set-option -g window-status-last-style "#[fg=${background},bg=${selection},nobold,noitalics,nounderscore]${left_sep} #[bg=${selection},fg=${green}] #I ${left_sep2} #W #[fg=${selection},bg=${background},nobold,noitalics,nounderscore]${left_sep}"
 }
 
 main
